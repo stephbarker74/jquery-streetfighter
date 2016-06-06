cc1ea985e5d7451342100517c88d72bad44b71c6
Stephanie Shipley <stephbarker74@yahoo.com>
2016-06-06 06:20:07 -0400

    Modified CSS & JS

--- js/app.js
+++ js/app.js
@@ -8,14 +8,42 @@ $(document).ready(function() {
 	$('.ryu-still').show();
   })
 	.mousedown(function() {
-    // play hadouken sound
-    $('.ryu-ready').hide();
-  	$('.ryu-throwing').show();
-  	$('.hadouken').show();
+      playHadouken();
+      $('.ryu-ready').hide();
+  	  $('.ryu-throwing').show();
+  	  $('.hadouken').finish().show()
+  		.animate(
+    	{'left': '1020px'},
+    	500,
+    	function() {
+      		$(this).hide();
+      		$(this).css('left', '520px');
+    	}
+  	);
     // animate hadouken to the right of the screen
   })
   .mouseup(function() {
    	$('.ryu-throwing').hide();
   	$('.ryu-ready').show();
   });
+
+  $("body").keydown(function( event ) {
+  	if (event.which === 88) {
+  		$('.ryu-still').hide();
+  		$('.ryu-ready').hide();
+  		$('.ryu-cool').show();
+  	}
+  });
+  $("body").keyup(function( event ) {
+  	if (event.which === 88) {
+  		$('.ryu-cool').hide();
+  		$('.ryu-still').show();
+  	}
+  });
 });
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
